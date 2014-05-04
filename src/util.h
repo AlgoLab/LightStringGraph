6e08e63a979bc5c81d5cf035554d91fff513139c
Yuri Pirola <yuri.pirola@gmail.com>
2014-05-04 21:06:44 +0200

    Add/improve preprocessor macros used for debugging/logging.

--- src/util.h
+++ src/util.h
@@ -3,11 +3,13 @@
 #define UTIL_H
 
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 #include <vector>
 #include <fstream>
 #include <sstream>
 #include <ctime>
+#include <stdexcept>
 
 #include "types.h"
 #include "edge_joined_interval.h"
@@ -47,18 +49,72 @@ ifstream& operator>>( ifstream&, GSAEntry& );
 std::string now( const char* format );
 
 #ifdef DEBUG
-#define DEBUG_LOG(s)                                        \
-  std::cerr << "DBG:" << __FILE__ << "(" << __LINE__ << ")" \
-  << " " << s << std::endl;
+#define DEBUG_LOG(s)                          \
+  do {                                        \
+    std::cerr << "DBG:"                       \
+              << __FILE__ << ":" << __LINE__  \
+              << "  " << s << std::endl;      \
+  } while (0)
 #else
-#define DEBUG_LOG(s) ;
+#define DEBUG_LOG(s) do {} while(0)
 #endif
 
 #ifdef DEBUG_VERBOSE
-#define DEBUG_LOG_VERBOSE(s)                                \
-  DEBUG_LOG(s)
+#define DEBUG_LOG_VERBOSE(s) DEBUG_LOG(s)
 #else
-#define DEBUG_LOG_VERBOSE(s) ;
+#define DEBUG_LOG_VERBOSE(s) do {} while(0)
+#endif
+
+
+
+#define QUOTE(str) #str
+#define EXPAND_AND_QUOTE(str) QUOTE(str)
+
+#define _MY_FAIL                               \
+  do {                                         \
+    DEBUG_LOG("Failure.");                     \
+    throw std::logic_error("Failure at " +     \
+                           std::string(__FILE__) + ":" +              \
+                           std::string(EXPAND_AND_QUOTE(__LINE__)) ); \
+  } while (0)
+
+#define _FAIL_AND_LOG( _MESSAGE_ )     \
+  do {                                 \
+    DEBUG_LOG( _MESSAGE_ );            \
+    throw std::logic_error(_MESSAGE_); \
+  } while (0)
+
+#define _FAIL_IF( _TEST_ )                    \
+  do {                                        \
+    if (_TEST_) {                             \
+      DEBUG_LOG("Condition '" <<              \
+                EXPAND_AND_QUOTE(_TEST_) <<   \
+                "' verified.");               \
+      _MY_FAIL;                               \
+    }                                         \
+  } while (0)
+
+
+
+#ifdef DEBUG  // DEBUG is ON
+
+#define PERFORM_AND_CHECK( TEST )                    \
+  do {                                               \
+    bool __test_perform_##__LINE__ = TEST;           \
+      if (!__test_perform_##__LINE__) {              \
+        DEBUG_LOG("Error while performing '" <<      \
+                  EXPAND_AND_QUOTE(TEST) << "'.");   \
+        _MY_FAIL;                                    \
+      }                                              \
+  } while(0)
+
+#else // DEBUG is OFF
+
+#define PERFORM_AND_CHECK( TEST )  \
+  do {                             \
+    TEST;                          \
+  } while(0)
+
 #endif
 
 #endif
