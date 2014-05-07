bd0a70742a647ae03c97c744d63b8d6513cdd0e5
Marco Previtali <m.previtali3@campus.unimib.it>
2014-05-07 17:39:55 +0200

    Minor fixes in src/extend_symbol_pile.cpp.
    - Use ios::out | ios::trunc instead of ios::out + remove files
    - Use const_iterator(s) insted of iterator(s)

--- src/extend_symbol_pile.cpp
+++ src/extend_symbol_pile.cpp
@@ -5,24 +5,27 @@
 ExtendSymbolPile::ExtendSymbolPile( vector< string >& filenames )
   : _filenamesIn( filenames )
 {
-  _mode = std::ios_base::out;
+  _mode = std::ios_base::out | std::ios_base::trunc;
   _init_files( );
 }
 
 ExtendSymbolPile::~ExtendSymbolPile( )
 {
-  for( vector< std::fstream* >::iterator it = _files.begin();
+  for( vector< std::fstream* >::const_iterator it = _files.begin();
        it != _files.end(); ++it)
     {
       (*it)->close();
       delete *it;
     }
-  _files.clear();
+  for( vector< string >::const_iterator it = _filenamesIn.begin();
+       it != _filenamesIn.end(); ++it )
+    if( remove((*it).c_str()) != 0 )
+        DEBUG_LOG( "Unable to remove file " << (*it).c_str() );
 }
 
 void ExtendSymbolPile::_init_files( )
 {
-  for( vector< std::fstream* >::iterator it = _files.begin();
+  for( vector< std::fstream* >::const_iterator it = _files.begin();
        it != _files.end(); ++it)
     {
       (*it)->close();
@@ -31,31 +34,17 @@ void ExtendSymbolPile::_init_files( )
 
   _files.clear( );
 
-  // Delete input files if new mode is input
-  if( _mode == std::ios_base::out )
-    {
-      for(vector< string >::iterator it = _filenamesIn.begin();
-          it != _filenamesIn.end(); ++it)
-        if( remove( (*it).c_str( ) )  != 0 )
-          {
-            DEBUG_LOG( "Unable to remove file " << (*it).c_str() );
-          }
-          
-    }
-
-  for(vector< string >::iterator it = _filenamesIn.begin();
+  for(vector< string >::const_iterator it = _filenamesIn.begin();
       it != _filenamesIn.end(); ++it )
-    {
-      _files.push_back( new std::fstream( (*it).c_str(), _mode ) );
-    }
+    _files.push_back( new std::fstream( (*it).c_str(), _mode) );
 }
 
 std::ios_base::openmode ExtendSymbolPile::switch_mode( )
 {
-  if( _mode == std::ios_base::out )
-      _mode = std::ios_base::in;
+  if( _mode == std::ios_base::in )
+    _mode = std::ios_base::out | std::ios_base::trunc;
   else
-    _mode = std::ios_base::out;
+    _mode = std::ios_base::in;
 
   _init_files( );
 
@@ -64,7 +53,7 @@ std::ios_base::openmode ExtendSymbolPile::switch_mode( )
 
 void ExtendSymbolPile::add_extend_symbol( vector< Nucleotide >& vnucl, const EdgeLength& l )
 {
-  assert( _mode == std::ios_base::in );
+  assert( _mode == (std::ios_base::out | std::ios_base::trunc) );
   (*_files[ l ]) << vnucl.size( );
   for( size_t i(0); i < vnucl.size(); ++i)
     (*_files[ l ]) << ntoc(vnucl[ i ]);
@@ -72,7 +61,7 @@ void ExtendSymbolPile::add_extend_symbol( vector< Nucleotide >& vnucl, const Edg
 
 vector< Nucleotide > ExtendSymbolPile::get_next_symbol( EdgeLength& l )
 {
-  assert( _mode == std::ios_base::out );
+  assert( _mode == std::ios_base::in );
   vector< Nucleotide > vnucl;
   size_t s;
   (*_files[ l ]) >> s;
