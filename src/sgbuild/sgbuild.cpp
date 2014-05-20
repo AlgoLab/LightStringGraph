bc00e9459fba31e51185a53ae5ca298a667a66bc
Marco Previtali <m.previtali3@campus.unimib.it>
2014-05-20 09:36:48 +0200

    Output ASQG format and various fixes.
    - Add better usage
    - Add -r parameter for reads file
    - Add -l parameter for reads length
    - Add -c parameter for edges count
    - Add -x parameter for exaustive output (overlap graph)
    - Use kseq.h when needed

--- src/sgbuild/sgbuild.cpp
+++ src/sgbuild/sgbuild.cpp
@@ -2,12 +2,17 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <zlib.h>
+#include <stdio.h>
 #include <cassert>
 
 #include "types.h"
 #include "util.h"
 #include "PrefixManager.h"
 #include "edgeLabelInterval.h"
+
+#include "kseq.h"
+KSEQ_INIT(gzFile, gzread)
 
 using std::string;
 using std::vector;
@@ -53,26 +58,92 @@ add_dest(struct Node* source, struct Node* dest, const QInterval& label, const S
 }
 
 void
-print_node(const struct Node n)
+show_usage(){
+  std::cerr << "Usage: sgbuild ";
+  std::cerr << " -b <basename> ";
+  std::cerr << " -m <maxarclen> ";
+  std::cerr << " [-c]";
+  std::cerr << " [-x]";
+  std::cerr << std::endl;
+  std::cerr << std::endl << "Parameters:" << std::endl;
+  std::cerr << "\t-b <basename>     # (required)" << std::endl;
+  std::cerr << "\t-m <maxarclen>    # (required)" << std::endl;
+  std::cerr << "\t[-c]                # (optional) count edges % default = false" << std::endl;
+  std::cerr << "\t[-x]                # (optional) exaustive   % default = false" << std::endl;
+  std::cerr << std::endl;
+}
+
+bool
+parse_cmds(int argc, char** argv, string& basename, string& readsfilename,
+           SequenceLength& maxarclen, int& readslen, bool& exaustive,
+           bool& countedg)
 {
-  assert(n._succs.size() == n._labels.size() && n._labels.size() == n._lens.size());
-  for(unsigned int i=0; i<n._succs.size(); ++i)
+  if(argc < 5)
     {
-      std::cout << n._succs[i]->_id << " -> " << n._id
-                << " [" << n._lens[i] << "]" << std::endl;
+      show_usage();
+      return false;
     }
+  for(int i=1; i<argc; ++i)
+    {
+      if(string(argv[i]) == "-b")
+        basename = string(argv[++i]);
+      else if(string(argv[i]) == "-r")
+        readsfilename = string(argv[++i]);
+      else if(string(argv[i]) == "-m")
+        maxarclen = atoi(argv[++i]);
+      else if(string(argv[i]) == "-l")
+        readslen = atoi(argv[++i]);
+      else if(string(argv[i]) == "-x")
+        exaustive = true;
+      else if(string(argv[i]) == "-c")
+        countedg = true;
+      else
+        {
+          std::cerr << "Can't parse argument: " << argv[i] << std::endl;
+          show_usage();
+          return false;
+        }
+    }
+
+  std::cerr << "Basename       : " << basename << std::endl
+            << "Reads file     : " << readsfilename << std::endl
+            << "Reads length   : " << readslen << std::endl
+            << "Max arcs len   : " << maxarclen << std::endl;
+  std::cerr << "Options: " << std::endl
+            << "   count-edges : ";
+  countedg ? std::cerr << "true" : std::cerr << "false";
+  std::cerr << std::endl;
+  std::cerr << "   exaustive   : ";
+  exaustive ? std::cerr << "true" : std::cerr << "false";
+  std::cerr << std::endl;
+
+  return true;
 }
 
 void
-show_usage(){
-  std::cerr << "Usage: sgbuild ";
-  std::cerr << " <basename> ";
-  std::cerr << " <maxarclen> ";
-  std::cerr << std::endl;
-  std::cerr << std::endl << "Parameters:" << std::endl;
-  std::cerr << "\t<basename>     # (required)" << std::endl;
-  std::cerr << "\t<maxarclen>    # (required)" << std::endl;
-  std::cerr << std::endl;
+print_vertex(kseq_t* seq)
+{
+  std::cout << "VT\t" << seq->name.s <<"\t" << seq->seq.s << std::endl;
+}
+
+void
+print_edge(vector< string >& ids, unsigned int source, unsigned int dest, int overlap, int readslen)
+{
+  // Fields:
+  // 0.  string ED
+  // 1.  sequence 1 name
+  // 2.  sequence 2 name
+  // 3.  sequence 1 overlap start (0 based)
+  // 4.  sequence 1 overlap end (inclusive)
+  // 5.  sequence 1 length
+  // 6.  sequence 2 overlap start (0 based)
+  // 7.  sequence 2 overlap end (inclusive)
+  // 8.  sequence 2 length
+  // 9.  sequence 2 orientation (1 for reversed with respect to sequence 1)
+  // 10. number of differences in overlap (0 for perfect overlaps, which is the default).
+  std::cout << "ED\t" << ids[source] << "\t" << ids[dest] << "\t" << readslen - overlap
+            << "\t" << readslen << "\t" << readslen << "\t0\t" << overlap << "\t" << readslen
+            << "\t0\t0" << std::endl;
 }
 
 //Main
@@ -80,19 +151,34 @@ show_usage(){
 int
 main(int argc, char** argv)
 {
-  if(argc != 3)
+  bool exaustive = false;
+  bool countedg  = false;
+  string basename, readsfilename;
+  int readslen =0;
+  SequenceLength maxarclen;
+  long edges = 0;
+
+  if(!parse_cmds(argc, argv, basename, readsfilename, maxarclen, readslen, exaustive, countedg))
+    return false;
+
+  vector< string > reads_ids;
+
+  gzFile fp;
+  kseq_t *seq;
+  fp = gzopen(readsfilename.c_str(), "r");
+  seq = kseq_init(fp);
+  while (kseq_read(seq) >= 0)
     {
-      show_usage();
-      return 1;
+      reads_ids.push_back(string(seq->name.s));
+      print_vertex(seq);
     }
+  kseq_destroy(seq);
+  gzclose(fp);
 
   vector< struct Node* > graph;
 
   vector< std::ifstream* > arcsFiles;
   vector< std::ifstream* > labelFiles;
-
-  string basename(argv[1]);
-  SequenceLength maxarclen = atoi(argv[2]);
 
   for(SequenceLength j(0); j < maxarclen; ++j)
     {
@@ -124,16 +210,28 @@ main(int argc, char** argv)
           pmgr.get_elems(sourcebegin, sourceend - sourcebegin, s_v);
           pmgr.get_elems(destbegin,   destend   - destbegin,   d_v);
           for(vector< SequenceNumber >::size_type i =0; i < s_v.size(); ++i)
-            for(vector< SequenceNumber >::size_type k =0; k < d_v.size(); ++k)
-              {
-                stretch(graph, std::max(s_v[i], d_v[k]));
-                add_dest(graph[s_v[i]], graph[d_v[k]], QInterval(labelbegin, labelend), j);
-              }
+            {
+              for(vector< SequenceNumber >::size_type k =0; k < d_v.size(); ++k)
+                {
+                  if(!exaustive)
+                    {
+                      stretch(graph, std::max(s_v[i], d_v[k]));
+                      add_dest(graph[s_v[i]], graph[d_v[k]], QInterval(labelbegin, labelend), j);
+                    }
+                  else
+                    print_edge(reads_ids, s_v[i], d_v[k], j, readslen);
+                  if(countedg)
+                    edges += ((sourceend - sourcebegin) * (destend - destbegin));
+                }
+            }
         }
     }
 
-  for(vector< struct Node* >::size_type i =0; i < graph.size(); ++i)
-      print_node(*graph[i]);
+  if(!exaustive)
+    for(unsigned int i =0; i < graph.size(); ++i)
+      for(unsigned int j=0; j < graph[i]->_succs.size(); ++j)
+        print_edge(reads_ids, graph[i]->_succs[j]->_id,
+                   graph[i]->_id, graph[i]->_lens[j], readslen);
 
   for(vector< struct Node* >::size_type i =0; i < graph.size(); ++i)
       delete graph[i];
@@ -143,6 +241,9 @@ main(int argc, char** argv)
 
   for(vector< std::ifstream* >::size_type i =0; i < labelFiles.size(); ++i)
     delete labelFiles[i];
+
+  if(countedg)
+    std::cerr << "Produced " << edges << " edges." << std::endl;
 
   return 0;
 }
