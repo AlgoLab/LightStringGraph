689129963b4c2f8887e14a527f387438e98229e0
Marco Previtali <m.previtali3@campus.unimib.it>
2014-05-26 14:48:39 +0200

    Add license

--- src/joined_q_int_manager.cpp
+++ src/joined_q_int_manager.cpp
@@ -1,3 +1,30 @@
+/**
+ *
+ *                     LightStringGraph
+ *
+ *            Lightweight String Graph Construction.
+ *
+ * Copyright (C) 2013, 2014 Stefano Beretta, Yuri Pirola, Marco Previtali
+ *
+ * Distributed under the terms of the GNU General Public License (or the Lesser
+ * GPL).
+ *
+ * This file is part of LightStringGraph.
+ *
+ * LighStringGraph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * LightStringGraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
 // #include <joined_q_int_manager.h>
 
 // // JoinedQIntervalManager::JoinedQIntervalManager ( vector< string >& filenames )
